3fbd8ca5bc74ba5978b3253f154161dfd4f63bd6
Dawid <dawidkotarba@users.noreply.github.com>
2016-11-12 23:48:48 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,23 +3,23 @@
 source ../dirs
 CONFIG=$CONFIG_PATH
 
-### SYMLINKS ###
-__create_symlink(){
+### CREATE SYMLINKS ###
+create_symlink(){
  local TARGET=$1
  local SHORTCUT=$2
  rm -rf $SHORTCUT
  ln -s $TARGET $SHORTCUT
 }
 
-__create_symlink $CONFIG/.gitconfig ~/.gitconfig 
-__create_symlink $CONFIG/.vim ~/.vim
-__create_symlink $CONFIG/.vimrc ~/.vimrc
+create_symlink $CONFIG/.gitconfig ~/.gitconfig 
+create_symlink $CONFIG/.vim ~/.vim
+create_symlink $CONFIG/.vimrc ~/.vimrc
 
-### APPS ###
-__clone_app(){
+### CLONE APPS ###
+clone_app(){
  (cd $BASH_APPS_PATH && git clone $1)
 }
 
-__clone_app https://github.com/rupa/z.git
-__clone_app https://github.com/nojhan/liquidprompt.git
+clone_app https://github.com/rupa/z.git
+clone_app https://github.com/nojhan/liquidprompt.git
 
