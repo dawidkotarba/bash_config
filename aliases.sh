4b5fc0d18fc28a6e2b39135c70f91c19a3c4e782
dawidkotarba <dawidkotarba>
2017-04-25 12:28:39 +0200

    update

--- aliases.sh
+++ aliases.sh
@@ -11,9 +11,6 @@ CURRENT_LOG_LVL=$LOG_LVL_INFO
 ### EXPORTS ###
 export TERM='xterm-256color'
 
-### BINDINGS ###
-bind 'set show-all-if-ambiguous on'
-
 ######################
 ## INITIAL SOURCING ##
 ######################
