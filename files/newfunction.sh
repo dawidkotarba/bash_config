bec5380af06031ba724e450b8d106ae385eae418
Dawid <dawidkotarba@users.noreply.github.com>
2017-08-07 21:38:13 +0200

    Update newfunction.sh

--- files/newfunction.sh
+++ files/newfunction.sh
@@ -1,6 +1,6 @@
 prefix-functionname(){
   [[ "$1" == "-h" ]] && __echo_info "Help information" && return
-  __check $1 "Check first argument message"
+  __check $1 "ParamName"
   local first_variable=$1
   # function body here
 }
