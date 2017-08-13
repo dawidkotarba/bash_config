498e746b2e896acbd18008f2e1f93296cbdd1450
dawidkotarba <dawidkotarba>
2017-08-13 19:20:02 +0200

    Update: modules/yy/aliases.sh,modules/yy/help.sh

--- modules/yy/aliases.sh
+++ modules/yy/aliases.sh
@@ -64,11 +64,13 @@ __check_if_hsqldb_is_used(){
 }
 
 __start-mysql-if-no-is_hsqldb(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   local is_hsqldb=`__check_if_hsqldb_is_used`
   [[ "$is_hsqldb" == 0 ]] && __check_and_start_hybris_mysql
 }
 
 __show_popup_if_hybris_has_started(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   local started=`tail $HYBRIS_LOG_PATH | grep "INFO: Server startup in"`
   [[ $started ]] && __show_popup "$started"
 }
@@ -77,9 +79,8 @@ __show_popup_if_hybris_has_started(){
 alias yy-start="__hybris-start"
 
 yy-setsuffix(){
-  [[ "$1" == "-h" ]] && __echo_info "sets up the hybris suffix" && return
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check $1 "hybris suffix"
-
  export HYBRIS_FOLDER_SUFFIX=$1
  export HYBRIS_HOME=$REPOSITORY_PATH/hybris_$HYBRIS_FOLDER_SUFFIX/hybris
  export HYBRIS_LOG_PATH=$HYBRIS_HOME/$HYBRIS_LOG
@@ -87,82 +88,90 @@ yy-setsuffix(){
 }
 
 yy-ps(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  local hybris_process=`__get_hybris_process`
  [[ ! $hybris_process ]] && __echo_err "HYBRIS IS NOT RUNNING" || __echo_ok "HYBRIS IS RUNNING: $hybris_process"
 }
 
 yy-navigate(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  cd $HYBRIS_HOME
 }
 
 yy-navigatecustom(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  cd $HYBRIS_HOME/bin/custom
 }
 
 yy-navigateconfig(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  cd $HYBRIS_HOME/config
 }
 
 yy-navigateplatform(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  cd $HYBRIS_HOME/bin/platform
 }
 
-# follow log
 yy-log(){
- [[ "$1" == "-h" ]] && __echo_info "Follow the hybris log file" && return
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  [[ $@ != *'-nolog'* ]] && tail -f $HYBRIS_LOG_PATH
 }
 
 yy-logclean(){
-  [[ "$1" == "-h" ]] && __echo_info "Clean the hybris log file" && return
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   > $HYBRIS_LOG_PATH
 }
 
-# grep log by arguments
 yy-grep(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   for var in "$@"
  do
   grep $var $HYBRIS_LOG_PATH
  done
 }
 
-# show errors and warnings from log
 yy-logerrorgrep(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  grep -E 'ERROR|WARN' $HYBRIS_LOG_PATH
 }
 
-# follow only error and warnings in log
 yy-logerrortail(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   tail -f $HYBRIS_LOG_PATH | grep -E 'ERROR|WARN'
 }
 
-# show log with less
 yy-logshow(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  less $HYBRIS_LOG_PATH
 }
 
-# follow tomcat log
 yy-logtomcat(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  (yy-navigate && cd log/tomcat && ls -lt | grep console | tail -1 | awk '{print $9}' | xargs tail -f)
 }
 
 yy-setantenv(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  (yy-navigateplatform && . ./setantenv.sh)
 }
 
 __yy-processes-to-kill(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  ps aux | grep hybris | grep -v $HYBRIS_LOG | grep -v atom | awk '{print $2}' | xargs kill
 }
 
 __yy-stop(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __on_hybris_platform sh hybrisserver.sh stop
 }
 
 yy-stop(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __echo_info "Before kill:"
  yy-ps
  local kill_executions=5
@@ -176,27 +185,30 @@ yy-stop(){
 }
 
 yy-visualvm(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  jvisualvm --openpid `__get_hybris_process` &
 }
 
 yy-jcmd(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  jcmd `__get_hybris_process` help
 }
 
 yy-configlocalproperties(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  atom $HYBRIS_LOCAL_PROPERTIES
 }
 
 yy-configlocalextensions(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  atom $HYBRIS_HOME/config/localextensions.xml
 }
 
-# create a new folder for hybris project
 yy-createproject(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check $1 "hybris project folder suffix"
-
   local hybris_folder_suffix=$1
   yy-setsuffix $hybris_folder_suffix
   echo yy-setsuffix $hybris_folder_suffix >> $BASH_AUTOSTART_PATH
@@ -205,125 +217,141 @@ yy-createproject(){
 
 ### HYBRIS ANT ###
 yy-req(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check $1 "revision back"
-
  local rev_back=$1
  local changes_count=`git diff --name-only HEAD..HEAD~$rev_back | egrep '.impex|items.xml' | wc -l`
  [[ $changes_count > 0 ]] && echo "ant initialize" || echo "ant clean all"
 }
 
 yy-jrebel(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check $1 "extension path"
  local ext_path=$1
  [[ $ext_path ]] && (cd $ext_path && ant build)
 }
 
 yy-grunt(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check $1 "extension path"
   local ext_path=$1
   (cd $ext_path && grunt)
 }
 
 yy-antshow(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __on_hybris_platform ant -p
 }
 
 yy-antall(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __on_hybris_platform ant all $@
 }
 
 yy-antallstart(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __on_hybris_platform_nolog ant all && yy-start
 }
 
 yy-antclean(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __on_hybris_platform ant clean
 }
 
 yy-antcleanall(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __on_hybris_platform ant clean all $@
 }
 
 yy-antcleanallstart(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __on_hybris_platform_nolog ant clean all && yy-start
 }
 
 yy-antbuild(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __on_hybris_platform ant build $@
 }
 
 yy-antupdatesystem(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __on_hybris_platform ant updatesystem $@
 }
 
 yy-antjunit(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __on_hybris_platform ant yunitinit
 }
 
 yy-antextgen(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  cd $HYBRIS_HOME/bin/platform
  ant extgen
 }
 
 yy-antmodulegen(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  cd $HYBRIS_HOME/bin/platform
  ant modulegen
 }
 
 yy-antinitialize(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __start-mysql-if-no-is_hsqldb
   __on_hybris_platform ant initialize $@
 }
 
 yy-antinitializestart(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __start-mysql-if-no-is_hsqldb
   __on_hybris_platform_nolog ant initialize && yy-start
 }
 
 yy-anttest(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __on_hybris_platform ant clean all yunitinit alltests
 }
 
 yy-antserver(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __on_hybris_platform ant server
 }
 
-# Copies required mysql driver to dbdriver folder
 yy-copydbdriver(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check_hybris_suffix
  cp ~/APPS/Hybris/mysql/mysql-connector-java-5.1.35-bin.jar $HYBRIS_HOME/bin/platform/lib/dbdriver
 }
 
 __yy-get_mysql_container_name(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   echo mysql_$HYBRIS_FOLDER_SUFFIX
 }
 
-# Starts docker mysql container
 yy-dockermysqlstart(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check_hybris_suffix
   kk-dockerstart $(__yy-get_mysql_container_name)
 }
 
-# Shows mysql docker IP
 yy-dockermysqlip(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check_hybris_suffix
   kk-dockerip $(__yy-get_mysql_container_name)
 }
 
-# Creates mysql docker container
-# Usage: yy-dockermysqlcreate 5.6.31
 yy-dockermysqlcreate(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check_hybris_suffix
   local mysql_version=""
   [[ $1 ]] && mysql_version=:$1
-
   sudo docker run --name $(__yy-get_mysql_container_name) -e MYSQL_ROOT_PASSWORD=root -d mysql/mysql-server$mysql_version --innodb_flush_log_at_trx_commit=0
 }
 
-# Creates a db and adds all privileges to the db to user.
 yy-dockermysqlcreatedbuser(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   [[ "$1" == "-h" ]] && __echo_info "yy-dockermysqlcreatedbuser dbName username userpassword" && return
   __check $1 "db name"
   __check $2 "user name"
