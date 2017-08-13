abfe4ed01224e35c8c676247643669c364c46168
dawidkotarba <dawidkotarba>
2017-08-13 18:32:15 +0200

    Update: modules/kk/aliases.sh,modules/kk/help.sh

--- modules/kk/aliases.sh
+++ modules/kk/aliases.sh
@@ -7,6 +7,7 @@ kk-clipboard(){
 alias clip='kk-clipboard'
 
 kk-server(){
+[[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  local port=7070
  __echo_info "Starting python server on $port"
  #python -m SimpleHTTPServer $port
@@ -15,25 +16,27 @@ kk-server(){
 
 ### Aliases edition ###
 kk-newfunction(){
-  [[ "$1" == "-h" ]] && __echo_info "Copies function template to clipboard" && return
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   kk-clipboard $BASH_NEW_FUNCTION_FILE
   __echo_ok "Function template copied to clipboard"
 }
 
 kk-newhelp(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   local help_code_line='[[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return'
   echo "$help_code_line" | xclip -selection clipboard
   __echo_ok "Help template copied to clipboard"
 }
 
 kk-newcheck(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   local check_code_line='__check $1 "paramName"'
   echo "$check_code_line" | xclip -selection clipboard
   __echo_ok "Check template copied to clipboard"
 }
 
 kk-bashnewmodule(){
- [[ "$1" == "-h" ]] && __echo_info "Creates a new module (modules/xxx/aliases.sh). Usage: kk-bashcreatemodule tst" && return
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check $1 "module_name"
  local module_name=$1
  local module_path=$BASH_MODULES_PATH/$module_name
@@ -43,46 +46,47 @@ kk-bashnewmodule(){
 }
 
 kk-bashedit(){
- [[ "$1" == "-h" ]] && __echo_info "Edits 'aliases.sh' file from provided module. If no module is specified, then edits a main 'aliases.sh' file." && return
-
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  local module_name=$1
  [[ $module_name ]] && atom $BASH_MODULES_PATH/$module_name/aliases.sh || atom $BASH_ALIASES_PATH
 }
 
 kk-bashedithelp(){
- [[ "$1" == "-h" ]] && __echo_info "Edits 'help.sh' file from provided module." && return
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __check $1 "module_name"
  local module_name=$1
  [[ $module_name ]] && atom $BASH_MODULES_PATH/$module_name/help.sh
 }
 
 kk-bashshow(){
- [[ "$1" == "-h" ]] && __echo_info "Shows 'aliases.sh' file from provided module. If no module is specified, then shows a main 'aliases.sh' file" && return
-
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  local module_name=$1
  [[ $module_name ]] && less $BASH_MODULES_PATH/$module_name/aliases.sh || less $BASH_ALIASES_PATH
 }
 
 kk-bashcommit(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  __git_add_commit_folder $BASH_CONFIG_PATH
 }
 
 kk-bashpush(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  git -C $BASH_CONFIG_PATH push
 }
 
 kk-bashcommitpush(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   kk-bashcommit
   kk-bashpush
 }
 
 kk-bashrevert(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   git -C $BASH_CONFIG_PATH checkout -f
 }
 
 __pull-cloned-apps(){
- [[ "$1" == "-h" ]] && __echo_info "Pulls all cloned apps in app folder." && return
-
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  local apps=$(ls $BASH_APPS_PATH)
  for app in $apps
   do
@@ -93,35 +97,36 @@ __pull-cloned-apps(){
 }
 
 kk-upgrade(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  apt update && apt upgrade -y
  git -C $BASH_CONFIG_PATH pull
  __pull-cloned-apps
 }
 
 kk-bak(){
-  [[ "$1" == "-h" ]] && __echo_info "Renames a subject by adding current date." && return
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check $1 "Subject of backup"
-
  local date=`date | awk '{print $3"-"$2"-"$4}'`
  mv $1 $1_$date
 }
 alias bak='kk-bak'
 
 kk-killall(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   local process_name=$1
   ps aux | grep $process_name | awk '{print $2}' | xargs kill
 }
 
 k(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __echo_ok "refreshing..."
   exec bash
 }
 
 ### DOCKER ###
 kk-dockerstart(){
-  [[ "$1" == "-h" ]] && __echo_info "Starts a docker container with provided name" && return
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check $1 "container name"
-
   local container_name=$1
   __echo_info "Starting docker container: $1"
   sudo docker start $container_name
@@ -129,23 +134,25 @@ kk-dockerstart(){
 }
 
 kk-dockerip(){
-  [[ "$1" == "-h" ]] && __echo_info "Prints IP of provided docker container" && return
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check $1 "container name"
-
   local container_name=$1
   sudo docker inspect $container_name | grep '"IPAddress"' | tail -n1
 }
 
 ### NAVIGATE ###
 kk-navigate(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   cd $BASH_CONFIG_PATH
 }
 
 kk-navigaterepo(){
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   cd $REPOSITORY_PATH
 }
 
 ### SCALA ###
 kk-createscala(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  cp $BASH_SCRIPTS_PATH/scala_script.sh .
 }
