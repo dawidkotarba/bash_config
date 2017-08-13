f6ef883baaf915869de0251e75548b22d61c53e1
dawidkotarba <dawidkotarba>
2017-08-13 19:49:11 +0200

    Update: modules/rpi/aliases.sh,modules/rpi/help.sh

--- modules/rpi/aliases.sh
+++ modules/rpi/aliases.sh
@@ -1,19 +1,22 @@
 ### RPI ###
 
 pssh(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  parallel-ssh -h $BASH_MODULES_PATH/rpi/pssh_hosts -t -1 -l pi -A $@
 }
 
 rpi-cluster-upgrade(){
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
  pssh sudo apt update && apt upgrade
 }
 
 rpi-wifiscan(){
-  sudo iwlist wlan0 scan
+ [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
+ sudo iwlist wlan0 scan
 }
 
 rpi-wifiadd(){
-  [[ "$1" == "-h" ]] && __echo_info "Saves wifi ssid/password. Usage: rpi-wifiadd testingssid testingpassword" && return
+  [[ "$1" == "-h" ]] && __show_help ${FUNCNAME[0]} && return
   __check $1 "ssid"
   __check $2 "password"
   wpa_passphrase $1 $2 >> /etc/wpa_supplicant/wpa_supplicant.conf
