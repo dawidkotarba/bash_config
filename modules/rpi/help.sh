e81e93c97b7df3394122c7ac509cc39a5df1754d
Dawid <dawidkotarba@users.noreply.github.com>
2017-08-17 22:22:10 +0200

    Update help.sh

--- modules/rpi/help.sh
+++ modules/rpi/help.sh
@@ -1,4 +1,4 @@
-pssh='Performs a command for all rpis in cluster'
+pssh='Performs a command for all rpis in cluster. Installation: apt install pssh'
 rpi_cluster_upgrade='Performs update and upgrade for all rpis in cluster'
 rpi_wifiscan='Scans wifi'
 rpi_wifiadd='Saves wifi ssid/password. Usage: rpi-wifiadd testingssid testingpassword'
