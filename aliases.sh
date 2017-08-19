86f120bdb376a0d9de0253834bd764240518da4f
dawidkotarba <dawidkotarba>
2017-08-19 09:28:39 +0200

    Update: aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -77,18 +77,9 @@ __echo_err(){
 __show_help(){
   __check $1 "Funciton name that is translated to related variable name"
   local help_variable_name=`echo $1 | tr - _`
-  # local help_message=${!help_variable_name}
-  echo "FUNCNAME: $help_variable_name"
+  # local help_message=${!help_variable_name} # bash
   local help_message=${(P)help_variable_name}
   [[ ! -z $help_message ]] && __echo_info $help_message || __echo_err "Help message unavailable"
-}
-
-tttest="testuj:)"
-
-tttest(){
-  local fname=$funcstack[1]
-  local help_message=${(P)${fname}}
-  echo $help_message
 }
 
 __generate_help(){
