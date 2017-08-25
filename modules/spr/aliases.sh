4f70227fe15738244e03d87273b4affca48a8078
dawidkotarba <dawidkotarba>
2017-08-26 00:13:55 +0200

    Update: modules/spr/aliases.sh

--- modules/spr/aliases.sh
+++ modules/spr/aliases.sh
@@ -1,5 +1,8 @@
-spr_newbean(){
+spr-newbean(){
   [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
   checkarg $1 "full class name"
-  python $SHELL_MODULES_PATH/spr/spr_newbean.py $1 | xclip -selection clipboard
+  local class_name=$1
+  if [[ -z $class_name ]] && class_name=`xclip -o`
+  python $SHELL_MODULES_PATH/spr/spr_newbean.py $class_name | xclip -selection clipboard
 }
+alias bean=spr-newbean
