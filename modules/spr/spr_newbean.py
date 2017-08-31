7484d881293344c1068c26df765de6d5fbbd8936
dawidkotarba <dawidkotarba>
2017-08-31 21:45:57 +0200

    Update: modules/spr/spr_newbean.py

--- modules/spr/spr_newbean.py
+++ modules/spr/spr_newbean.py
@@ -5,7 +5,7 @@ beanClass = sys.argv[1]
 m = re.search('\.(\w*$)', beanClass)
 decapitalize = lambda s: s[:1].lower() + s[1:] if s else ''
 beanName = decapitalize(m.group(1))
-beanDef = '<ban name="' + beanName + '" class="' + beanClass + '" />'
+beanDef = '<bean name="' + beanName + '" class="' + beanClass + '" />'
 aliasDef = '<alias name="' + beanName + '" alias="' + beanName + '" />'
 print(aliasDef + "\n" + beanDef)
 # usage: python test.py org.Test | xclip -selection clipboard
