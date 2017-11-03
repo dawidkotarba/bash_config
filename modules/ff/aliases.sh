85cd0dc5b247778663bd8670258e8d67ca997a5a
dawidkotarba <dawidkotarba>
2017-11-04 00:02:23 +0100

    Update: modules/ff/aliases.sh

--- modules/ff/aliases.sh
+++ modules/ff/aliases.sh
@@ -3,17 +3,17 @@
 ff-vim(){
   [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
   checkarg $1 "Search argument"
-  vim `find . -name "*$1*" | fzy`
+  vim `find . -name "*$1*" -type f | fzy`
 }
 
 ff-less(){
   [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
   checkarg $1 "Search argument"
-  less `find . -name "*$1*" | fzy`
+  less `find . -name "*$1*" -type f | fzy`
 }
 
 ff-atom(){
   [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
   checkarg $1 "Search argument"
-  atom `find . -name "*$1*" | fzy`
+  atom `find . -name "*$1*" -type f | fzy`
 }
