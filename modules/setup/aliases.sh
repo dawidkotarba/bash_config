4c6b5e99d2a4b95d80e8889ca57687f69fb0eda4
dawidkotarba <dawidkotarba>
2017-11-03 22:44:31 +0100

    Update: modules/setup/aliases.sh

--- modules/setup/aliases.sh
+++ modules/setup/aliases.sh
@@ -3,7 +3,7 @@
 _aptinstall(){
    [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
    checkarg $1 "at least one program to install"
-   echo_info "Installing $@"
+   echo_info "Installing: $@"
    apt install $@
 }
 
@@ -18,4 +18,16 @@ setup-pidgin(){
 
 setup-evolution(){
   _aptinstall evolution evolution-ews
+}
+
+setup-atom(){
+  _aptinstall atom
+
+  echo_info "Installing: plugins"
+  apm install minimap highlight-selected simple-drag-drop-text git-plus merge-conflicts
+  apm install color-picker pigments file-icons intellij-idea-keymap
+
+  #js
+  apm install atom-beautify linter-csslint linter-htmlhint linter-eslint linter-less
+  apm install linter-lesshint sort-lines editorconfig emmet
 }
