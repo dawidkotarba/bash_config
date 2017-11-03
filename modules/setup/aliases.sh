7cbfcc6bf7d6e69d094c6adbc57c334e4da705f7
dawidkotarba <dawidkotarba>
2017-11-03 20:55:41 +0100

    Update: modules/setup/aliases.sh

--- modules/setup/aliases.sh
+++ modules/setup/aliases.sh
@@ -1,6 +1,21 @@
 ### setup ###
 
+_aptinstall(){
+   [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
+   checkarg $1 "at least one program to install"
+   echo_info "Installing $@"
+   apt install $@
+}
+
 setup-ranger(){
-  apt install ranger caca-utils highlight atool w3m poppler-utils mediainfo
+  _aptinstall ranger caca-utils highlight atool w3m poppler-utils mediainfo
   ranger --copy-config=all
+}
+
+setup-pidgin(){
+  _aptinstall pidgin pidgin-sipe
+}
+
+setup-evolution(){
+  _aptinstall evolution evolution-ews
 }
