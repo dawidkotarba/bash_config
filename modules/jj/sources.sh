71cfc255dedd01fb6def88f9f60a974dc81c2957
dawidkotarba <dawidkotarba>
2017-11-22 12:16:15 +0100

    move spr to jj

--- modules/jj/sources.sh
+++ modules/jj/sources.sh
@@ -12,3 +12,12 @@ jj-findinjar() {
     fi
   done
 }
+
+jj-newbean(){
+  [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
+  checkarg $1 "full class name"
+  local class_name=$1
+  if [[ -z $class_name ]] && class_name=`xclip -o`
+  python $_SHELL_MODULES_PATH/jj/files/spring_newbean.py $class_name | xclip -selection clipboard
+}
+alias bean=jj-newbean
--- modules/spr/sources.sh
+++
@@ -1,8 +0,0 @@
-spr-newbean(){
-  [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
-  checkarg $1 "full class name"
-  local class_name=$1
-  if [[ -z $class_name ]] && class_name=`xclip -o`
-  python $_SHELL_MODULES_PATH/spr/files/spr_newbean.py $class_name | xclip -selection clipboard
-}
-alias bean=spr-newbean
