88aed918172d520cc9f18dc5326f6eae927b9872
dawidkotarba <dawidkotarba>
2018-05-06 23:40:08 +0200

    Update: modules/setup/sources.sh

--- modules/setup/sources.sh
+++ modules/setup/sources.sh
@@ -101,6 +101,13 @@ setup-socials(){
   _aptinstall corebird
 }
 
+setup-ripgrep(){
+  [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
+  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
+  sudo dpkg -i ripgrep_0.8.1_amd64.deb
+  rm ripgrep_0.8.1_amd64.deb
+}
+
 setup-funny(){
   [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
   _aptinstall figlet cmatrix
