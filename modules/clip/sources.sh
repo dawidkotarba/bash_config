bd8abfb1cb0e5399955531fc1a43f80f400e392c
dawidkotarba <dawidkotarba>
2018-06-07 16:47:35 +0200

    Update: modules/clip/sources.sh

--- modules/clip/sources.sh
+++ modules/clip/sources.sh
@@ -1,7 +1,24 @@
 ### clip ###
+_requires xclip
+
+_get-desktop-path(){
+  echo `xdg-user-dir DESKTOP`
+}
+
+_get-date(){
+  echo `date +%Y-%m-%d_%T`
+}
+
+clip-targets(){
+  xclip -selection clipboard -t TARGETS -o
+}
 
 clip-toimage(){
   [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
-  local desktopPath=`xdg-user-dir DESKTOP`
-  xclip -selection clipboard -t image/png -o > "$desktopPath/`date +%Y-%m-%d_%T`.png"
+  xclip -selection clipboard -t image/png -o > "`_get-desktop-path`/`_get-date`.png"
+}
+
+clip-totext(){
+  [[ "$1" == "-h" ]] && show_help $funcstack[1] && return
+  xclip -selection clipboard -o > "`_get-desktop-path`/`_get-date`.txt"
 }
