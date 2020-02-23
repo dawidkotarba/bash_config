c1173f9c00850df89ba99dd81064aa406772a818
dawidkotarba <dawidkotarba>
2020-02-23 15:24:19 +0100

    Update path file

--- path.sh
+++ path.sh
@@ -1,5 +1,5 @@
 ### Example of adding to path: _pathadd "/etc/scala/bin"
-_pathadd "/etc/scala/bin"
+# _pathadd "/etc/scala/bin"
 
 # EXPORT
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+# export JAVA_HOME=/usr/lib/jvm/java-8-oracle
