caff7b82dd13e5b6f2122e2783d69e1fc7345ab1
dawidkotarba <dawidkotarba>
2020-02-23 14:29:28 +0100

    Update autostart file

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,5 @@
 # Environment variables:
 # export COMPUTERNAME=komp
-# export HYBRIS_HOME=/home/dawidkotarba/_repository/hybris_sfx/hybris
 
 # Execute the autostarts commands:
-yy-init
-# yy-init sfx if no HYBRIS_HOME defined
+# autostart_command
