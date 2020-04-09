91f699cb3c6aa7c2f67930c3c9960e06f19ef2ab
dawidkotarba <dawidkotarba>
2020-04-09 10:57:02 +0200

    Update: modules/goals/sources.sh

--- modules/goals/sources.sh
+++ modules/goals/sources.sh
@@ -1,8 +1,38 @@
 ### goals ###
 
-goals-edit(){
+nav-goals(){
   _help $1 && return
   nav-repo
-  cd personal-goals/diary
+  cd personal-goals
+}
+
+nav-diary(){
+  _help $1 && return
+  nav-goals
+  cd diary
+}
+
+goals-edit(){
+  _help $1 && return
+  nav-diary
   ./edit.sh
+}
+
+goals-commit(){
+  _help $1 && return
+  nav-goals
+  git add .
+  git commit -m "Update goals"
+}
+
+goals-push(){
+  _help $1 && return
+  nav-goals
+  git push
+}
+
+goals-commitpush(){
+  _help $1 && return
+  goals-commit
+  goals-push
 }
