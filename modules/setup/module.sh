2ae0c79edb53bc43ef3779670b949b2a292a47f1
dawidkotarba <dawidkotarba>
2020-08-06 23:51:06 +0200

    Update: modules/setup/module.sh

--- modules/setup/module.sh
+++ modules/setup/module.sh
@@ -175,7 +175,7 @@ setup-essentials(){
   _aptinstall htop glances
 
   # Linux essentials
-  _aptinstall tilda vim tree mtr net-tools shellcheck samba
+  _aptinstall tilda vim tree mtr net-tools shellcheck samba xclip
 
   # SSH
   _aptinstall sshpass
