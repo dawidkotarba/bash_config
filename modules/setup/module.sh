630a945104387657864f2dccc566ccd42b79eddd
dawidkotarba <dawidkotarba>
2020-12-06 12:39:52 +0100

    Update: modules/setup/module.sh

--- modules/setup/module.sh
+++ modules/setup/module.sh
@@ -272,12 +272,10 @@ setup-woeusb(){
 
 setup-enpass(){
   _help $1 && return
-  sudo -i
-  echo "deb https://apt.enpass.io/ stable main" > /etc/apt/sources.list.d/enpass.list
-  wget -O - https://apt.enpass.io/keys/enpass-linux.key | apt-key add -
+  sudo echo "deb https://apt.enpass.io/ stable main" > /etc/apt/sources.list.d/enpass.list
+  sudo wget -O - https://apt.enpass.io/keys/enpass-linux.key | apt-key add -
   _aptupdate
   _aptinstall enpass
-  exit
 }
 
 setup-new-daw(){
